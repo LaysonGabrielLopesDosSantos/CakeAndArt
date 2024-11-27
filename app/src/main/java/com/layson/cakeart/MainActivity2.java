1af407e447748068f01fc0135b7b02bec936a40a
LaysonGabrielLopesDosSantos <laysongabriellopesdossantos@gmail.com>
2024-11-27 10:03:25 -0300

    Telas + Placeholder

--- app/src/main/java/com/layson/cakeart/MainActivity2.java
+++ app/src/main/java/com/layson/cakeart/MainActivity2.java
@@ -2,7 +2,7 @@ package com.layson.cakeart;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +10,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        new Handler().postDelayed(() -> {
-            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-            startActivity(intent);
-            finish();
-        }, 1000);
-
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    public void AtivePerfil() {
+        Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+        startActivity(intent);
+        finish();
+    }
+    public void AtiveMore() {
+        Intent intent = new Intent(MainActivity2.this, MainActivity4.class);
+        startActivity(intent);
+        finish();
+    }
+    public void AtiveMain() {
+        Intent intent = new Intent(MainActivity2.this, MainActivity5.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
--- app/src/main/java/com/layson/cakeart/MainActivity3.java
+++ app/src/main/java/com/layson/cakeart/MainActivity3.java
@@ -1,8 +1,7 @@
 package com.layson.cakeart;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,21 +9,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        new Handler().postDelayed(() -> {
-            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-            startActivity(intent);
-            finish();
-        }, 1000);
-
+        setContentView(R.layout.activity_main3);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
